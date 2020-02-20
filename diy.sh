ca233fa9de6944fe6b0bcfc0b0b5e8c648c5cfb6
Hello kitty <my1g2m3.gmail.com>
2020-02-20 18:04:25 +0800

    update config
    update

--- diy.sh
+++ diy.sh
@@ -13,9 +13,6 @@ sed -i 's/config internal themes/config internal themes\n    option Argon  \"\/l
 echo '去除默认bootstrap主题'
 sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
 
-echo '删除旧版argon,链接新版'
-rm -rf ./package/lean/luci-theme-argon
-ln -s ../../../luci-theme-argon ./package/lean/
 
 echo '修改wifi名称'
 sed -i 's/OpenWrt/my_home/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
@@ -24,11 +21,8 @@ echo '修改banner'
 rm -rf package/base-files/files/etc/banner
 cp -f ../banner package/base-files/files/etc/
 
-echo '下载ServerChan'
-git clone https://github.com/tty228/luci-app-serverchan ../diy/luci-app-serverchan
+#echo '下载ServerChan'
+#git clone https://github.com/tty228/luci-app-serverchan ../diy/luci-app-serverchan
 
 echo '集成diy目录'
 ln -s ../../diy ./package/openwrt-packages
-
-
-
